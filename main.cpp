4388866afd24ace7dad2a4bfb22b776833b8bbf5
KoKoLates <the21515@gmail.com>
2024-01-12 12:06:42 +0800

    chore: refactor main w/o `goto` syntax

--- main.cpp
+++ main.cpp
@@ -1,37 +1,49 @@
-#include <string>
 #include <fstream>
+#include <sstream>
 #include <iostream>
+#include <stdexcept>
 
 #include "VideoPipeline.hpp"
 
+class JsonReader {
+public:
+    JsonConfigReader(const std::string& filePath): input(filePath) {
+        if (!input.is_open()) {
+            throw std::runtime_error("Failed to open configure file");
+        }
+        contents = { std::istreambuf_iterator<char>(input), std::istreambuf_iterator<char>() };
+    }
+
+    Json::Value parseJson() {
+        Json::Reader reader;
+        Json::Value value;
+
+        if (!reader.parse(contents, value)) {
+            throw std::runtime_error("Failed parsing json");
+        }
+        return value;
+    }
+
+private:
+    std::ifstream input;
+    std::string contents;
+};
+
 int main(int argc, char **argv) {
-    Json::Reader reader;
-    std::ifstream input("/root/trip2-snpe/cfg/config.json");
-    if (!input.is_open()) {
-        std::cerr << "[error] failed to open configure files" << std::endl;
+    try {
+        JsonReader configReader("/root/trip2-snpe/cfg/config.json");
+        Json::Value value = configReader.parseJson();
+
+        VideoPipeline m_vp(value);
+        if (!m_vp.create(value["model"]["path"])) {
+            std::cerr << "[error] failed to create video pipeline instance." << std::endl;
+            return 1;
+        }
+        m_vp.start();
+    } catch (const std::exception& ex) {
+        std::cerr << "[error] " << ex.what() << std::endl;
         return 1;
     }
-    std::string contents((std::istreambuf_iterator<char>(input)),
-                          std::istreambuf_iterator<char>());
-    input.close();
 
-    Json::Value value;
-    if (!reader.parse(contents, value)) {
-        std::cerr << "[error] failed parsing json" << std::endl;
-        return 1;
-    }
-
-    VideoPipeline *m_vp = new VideoPipeline(value);
-    if (!m_vp->create("/root/trip2-snpe/yolov7_tiny.dlc")) {
-        std::cerr << "[error] failed to create video pipeline instance." << std::endl;
-        goto exit;
-    }
-    m_vp->start();
-
-exit:
-    if (m_vp) {
-        delete m_vp;
-        m_vp = nullptr;
-    }
     return 0;
 }
